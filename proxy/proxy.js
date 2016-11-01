5592036abbaa2f899788841bff18dbb4bcebf462
ha10id <ha10id@cit-sk.ru>
2016-11-01 11:21:05 +0300

    ed

--- proxy/proxy.js
+++
@@ -1,4 +0,0 @@
-var proxy = require('redbird')({port: 80, xfwd: false});
-
-// proxy.register("wks-0014.apache", "http://localhost:8080");
-proxy.register("wks-0014.local", "http://localhost:3000");
